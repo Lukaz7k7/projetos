02d1fe4c018bf9e7853308e1556c1dbe4a809d8c
Lukaz7k7 <lucasantoniofb11@gmail.com>
2026-06-22 10:56:26 -0300

    commit

--- projeto_sistema_motel.py
+++ projeto_sistema_motel.py
@@ -1,7 +1,35 @@
 import os , time 
 from datetime import datetime
+def verifica_cpf(cpf):
+    cpf = cpf.replace('.', '')
+    cpf = cpf.replace('-', '')
+    cpf = cpf.replace(' ', '')
+    if len(cpf) != 11:
+        return False
+    for caractere in cpf:
+        if caractere < '0' or caractere > '9':
+            return False
+    if cpf == cpf[0] * 11:
+        return False
+    soma = 0
+    for i in range(9):
+        soma += int(cpf[i]) * (10 - i)
+    d1 = 11 - (soma % 11)
+    if d1 >= 10:
+        d1 = 0
+    soma = 0
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+    d2 = 11 - (soma % 11)
+    if d2 >= 10:
+        d2 = 0
+    if d1 == int(cpf[9]) and d2 == int(cpf[10]):
+        return True
+    return False
 
-   # recuperando dados dos arquivos
+
+    # recuperando dados dos arquivos
+
 suites = {}
 try : 
         arq_suites = open('suites.txt','rt', encoding="utf-8")
@@ -60,7 +88,7 @@ try :
                 num = int(dados[0])
                 suite = int(dados[1])
                 entrada = datetime.fromisoformat(dados[2])
-                cpf = int(dados[3])
+                cpf = dados[3]
                 status = dados[4]
                 if dados[4] == 'fechado':
                     saida = datetime.fromisoformat(dados[5])
@@ -446,7 +474,11 @@ while resp != 0 :
               while suite not in suites or (suites[suite]['status'] != "livre" and suites[suite]['ativo'] == False) :
                   print('suíte invalida, digite uma suíte disponivel.')
                   suite = int(input('ָ☾. digite o numero da suite que deseja : '))
-              cpf = int(input('ָ☾. digite o seu CPF : '))
+              cpf = input('ָ☾. digite o seu CPF : ')
+              while verifica_cpf(cpf) == False :
+                print('      ! CPF INVALIDA !')
+                cpf = input('ָ☾. digite o seu CPF : ')
+
               entrada = datetime.now()
               print()
               hospedagens[num] = {
